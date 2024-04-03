4fb80988dafb798f6d7c92f5b7be4e8fa493a3f9
Kavya Sonkar <kavyasonkar10@gmail.com>
2024-04-03 18:01:31 +0200

    Buttons Fixed own

--- src/App.js
+++ src/App.js
@@ -1,65 +1,65 @@
-import React, { useState } from 'react'; 
-import './App.css';
-import About from './components/About';
-import Alert from './components/Alert';
-import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
+import React, { useState } from "react";
+import "./App.css";
+import About from "./components/About";
+import Alert from "./components/Alert";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-
 function App() {
-const [mode, setMode] = useState('light');//weather dark mode is enabled or not
-const [alert, setAlert] = useState(null);
+  const [mode, setMode] = useState("light"); //weather dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
 
-const showAlert= (message,type)=>{
-  setAlert({
-    msg: message,
-    type: type
-  })
-  setTimeout(() => {
-    setAlert(null);
-  },1500);
-}
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
 
-const toggleMode = () =>{
-  if(mode ==='light'){
-  setMode('dark')
-  document.body.style.backgroundColor= '#042743';
-  showAlert('Dark mode has been enabled','success');
-  document.title =  'TextUtils - Dark Mode';
-  // setInterval(() => { 
-  //   document.title =  'TextUtils is amazing';
-  // }, 2000);
-  // setInterval(() => {
-  //   document.title =  'Download now!!!';
-  // }, 1500);
-}
-  else{
-    setMode('light')
-    document.body.style.backgroundColor= 'white';
-    showAlert('Light mode has been enabled','success');
-    document.title =  'TextUtils - Light Mode';
-  }
-}
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+      showAlert("Dark mode has been enabled", "success");
+      document.title = "TextUtils - Dark Mode";
+      setInterval(() => {
+        document.title = "TextUtils is amazing";
+      }, 2000);
+      setInterval(() => {
+        document.title = "Download now!!!";
+      }, 1500);
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled", "success");
+      document.title = "TextUtils - Light Mode";
+    }
+  };
 
   return (
     <>
-    {/* <Navbar tittle='Textkavya' AboutText="about us hai"/> */}
-  <Router>
-  <Navbar tittle='TextUtils' mode={mode} toggleMode={toggleMode}/>
-  <Alert alert={alert}/>
-  <div className="container">
+      <Router>
+        <Navbar
+          tittle="TextUtils"
+          mode={mode}
+          toggleMode={toggleMode}
+          showAlert={showAlert}
+        />
+        <Alert alert={alert} />
+        <div className="container">
           <Routes>
-          <Route exact path="/about" element={<About mode={mode}/>}></Route>
-          <Route exact path="/" element={<TextForm heading="Try textUtils - Word counter, Character counter, Remove extra spaces"  mode={mode} showAlert={showAlert} />}></Route>
+            <Route exact path="/" element={<TextForm heading="Try textUtils - Word counter, Character counter, Remove extra spaces" mode={mode} showAlert={showAlert} />}></Route>
+            <Route exact path="/about" element={<About mode={mode} />}></Route>
           </Routes>
           {/* <TextForm heading="Enter text to analyze"  mode={mode} showAlert={showAlert} /> */}
-          
         </div>
-  </Router>
-  </>
-    );
+      </Router>
+    </>
+  );
 }
 
 export default App;
- 
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,103 +1,103 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 
 export default function TextForm(props) {
 
-    const handleUpClick = ()=>{
-        //console.log('Uppercase was clicked' + text);
-        let newText=text.toUpperCase();
-        setText(newText);
-        props.showAlert('Text has been capitalised','success');
-    }
-
-    const handleLoClick = ()=>{
-      let newText=text.toLowerCase();
-      setText(newText);
-      props.showAlert('Text has been lowercased','success');
-    }
-
-    const handleClearClick = ()=>{
-      let newText="";
-      setText(newText);
-      props.showAlert('text cleared','success');
-    }
-
-    const speak = () => {
-      let msg = new SpeechSynthesisUtterance();
-      msg.text = text;
-      window.speechSynthesis.speak(msg);
-      props.showAlert('Speaking now','success');
-    }
-
-    const speakstop = () => {
-      let msg = new SpeechSynthesisUtterance(text);
-      window.speechSynthesis.speak(msg);
-      const toogle = document.getElementById('toggle')
-      if (toogle.textContent == "Speak") {
-          toogle.innerHTML = "Stop"
-          props.showAlert('Speaking now','success');
-      }
-      else {
-          toogle.innerHTML = "Speak"
-          if (toogle.innerHTML = "Speak"){
-              window.speechSynthesis.cancel()
-              props.showAlert('stop','success');
-          }
-      }
+  const handleUpClick = () => {
+    //console.log('Uppercase was clicked' + text);
+    let newText = text.toUpperCase();
+    setText(newText);
+    props.showAlert('Text has been capitalised', 'success');
   }
 
-    const handleReverse = (event) => {
-      /* Convert string to array*/
-      let strArr = text.split("");
-      /* Reverse array*/
-      strArr = strArr.reverse();
-      /* Convert array to string*/
-      let newText = strArr.join("");
-      setText(newText);
-    };
- 
-    const reversed = () => {
-      let splitWord = text.split("");
-      let reverseWord = splitWord.reverse("");
-      let joinedWords = reverseWord.join("");
-      let newText = joinedWords
-      setText(newText);
-      props.showAlert('text reversed','success');
-    };
+  const handleLoClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+    props.showAlert('Text has been lowercased', 'success');
+  }
 
+  const handleClearClick = () => {
+    let newText = "";
+    setText(newText);
+    props.showAlert('text cleared', 'success');
+  }
 
-    const handleOnChange = (event)=>{
-        //console.log('on change');
-        setText(event.target.value);
+  // const speak = () => {
+  //   let msg = new SpeechSynthesisUtterance();
+  //   msg.text = text;
+  //   window.speechSynthesis.speak(msg);
+  //   props.showAlert('Speaking now', 'success');
+  // }
+
+  const speakstop = () => {
+    let msg = new SpeechSynthesisUtterance(text);
+    window.speechSynthesis.speak(msg);
+    const toogle = document.getElementById('toggle')
+    if (toogle.textContent == "Speak") {
+      toogle.innerHTML = "Stop"
+      props.showAlert('Speaking now', 'success');
     }
+    else {
+      toogle.innerHTML = "Speak"
+      if (toogle.innerHTML = "Speak") {
+        window.speechSynthesis.cancel()
+        props.showAlert('stop', 'success');
+      }
+    }
+  }
 
-    const [text, setText] = useState('');
-    //setText('new text');
+  const handleReverse = (event) => {
+    /* Convert string to array*/
+    let strArr = text.split("");
+    /* Reverse array*/
+    strArr = strArr.reverse();
+    /* Convert array to string*/
+    let newText = strArr.join("");
+    setText(newText);
+  };
+
+  // const reversed = () => {
+  //   let splitWord = text.split("");
+  //   let reverseWord = splitWord.reverse("");
+  //   let joinedWords = reverseWord.join("");
+  //   let newText = joinedWords
+  //   setText(newText);
+  //   props.showAlert('text reversed', 'success');
+  // };
+
+
+  const handleOnChange = (event) => {
+    //console.log('on change');
+    setText(event.target.value);
+  }
+
+  const [text, setText] = useState('');
+  //setText('new text');
   return (
     <>
-    <div className='container' style={{color:props.mode==='light'?'black':'white'}}>
-        <h1 className='mb-5'>{props.heading}</h1> 
+      <div className='container' style={{ color: props.mode === 'light' ? 'black' : 'white' }}>
+        <h1 className='mb-5'>{props.heading}</h1>
         <div className="mb-1">
-        <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor:props.mode==='dark'?'rgb(36 74 104)':'white', color:props.mode==='light'?'rgb(36 74 104)':'white'}}  id="myBox" rows="8"></textarea>
+          <textarea className="form-control" value={text} onChange={handleOnChange} style={{ backgroundColor: props.mode === 'dark' ? 'rgb(36 74 104)' : 'white', color: props.mode === 'light' ? 'rgb(36 74 104)' : 'white' }} id="myBox" rows="8"></textarea>
         </div>
-        <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={handleUpClick}>Convert to UPPERCASE</button>
-        <button disabled={text.length===0}className="btn btn-primary mx-2 my-2" onClick={handleLoClick}>Convert to lowercase</button>
-        <button disabled={text.length===0}className="btn btn-primary mx-2 my-2" onClick={handleClearClick}>Clear text</button>
-        <button disabled={text.length===0}type="submit" onClick={speak} className="btn btn-warning mx-2 my-2">Speak</button>
-        <button disabled={text.length===0}type="submit" onClick={speakstop} className="btn btn-warning mx-2 my-2" id="toggle">Speak stop</button>
-        <button disabled={text.length===0}type="submit" onClick={handleReverse} className="btn btn-warning mx-2 my-2">Reverse</button>
-        <button disabled={text.length===0}type="submit" onClick={reversed} className="btn btn-warning mx-2 my-2">reverse2</button>
-    </div>
+        <button disabled={text.length === 0} className="btn btn-primary mx-2 my-2" onClick={handleUpClick}>Convert to UPPERCASE</button>
+        <button disabled={text.length === 0} className="btn btn-primary mx-2 my-2" onClick={handleLoClick}>Convert to lowercase</button>
+        <button disabled={text.length === 0} className="btn btn-primary mx-2 my-2" onClick={handleClearClick}>Clear text</button>
+        {/* <button disabled={text.length === 0} className="btn btn-primary mx-2 my-2" onClick={speak} >Speak</button> */}
+        <button disabled={text.length === 0} className="btn btn-primary mx-2 my-2" onClick={speakstop} id="toggle">Speak/Stop</button>
+        <button disabled={text.length === 0} className="btn btn-primary mx-2 my-2" onClick={handleReverse} >Reverse</button>
+        {/* <button disabled={text.length === 0} className="btn btn-primary mx-2 my-2" onClick={reversed} >Reverse2</button> */}
+      </div>
 
-    <div className='container my-3' style={{color:props.mode==='light'?'black':'white'}}>
-     <h3>Your summary text</h3>  
-     <p>{text.split(' ').filter((element) => {return element.length != 0}).length} words and {text.length} characters</p>
-     {/* <p>{text.split(' ').length} words and {text.length} characters</p> */}
-     <p>{0.08 * text.split(" ").filter((element) => {return element.length != 0}).length} minutes read</p>
-     <h3>Preview</h3>
-     <p>{text.length>0?text:'Enter something in textbox above to preview it here'}</p>
-    </div>
+      <div className='container my-3' style={{ color: props.mode === 'light' ? 'black' : 'white' }}>
+        <h3>Your summary text</h3>
+        <p>{text.split(' ').filter((element) => { return element.length != 0 }).length} words and {text.length} characters</p>
+        {/* <p>{text.split(' ').length} words and {text.length} characters</p> */}
+        <p>{0.08 * text.split(" ").filter((element) => { return element.length != 0 }).length} minutes read</p>
+        <h3>Preview</h3>
+        <p>{text.length > 0 ? text : 'Enter something in textbox above to preview it here'}</p>
+      </div>
     </>
-    
-  ) 
+
+  )
 }
